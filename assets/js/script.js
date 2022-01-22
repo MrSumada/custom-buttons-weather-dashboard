ed25a5dcaa5030fcd193534f67eeb8c754dea5ce
MrSumada <ricksamericana.com>
2022-01-21 22:51:24 -0500

    add api fetch functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,61 @@
+// var city;
+
+// fetch(
+//     'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely&appid=d3c47a1f177d224c8f7fe16686ddb65e'
+//     ).then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+// });
+
+// fetch("https://api.myptv.com/geocoding/v1/locations/by-text?searchText=Los%20Angeles", {
+//         method: "GET",
+//         headers: { apiKey: "Y2E4ODI1NGU1MjlhNGFmODllN2VhYTQ0NzM4ZWUzZDM6MjAwYmZlN2UtZWYzNi00ZDIyLTkzNjEtNjFiMGU2MmE4NGY3", "Content-Type": "application/json" },
+//     })
+//     .then(response => response.json())
+//     .then(result => console.log(result.locations[0].referencePosition));
 
 
-var city;
+$("#searchBtn").on("click", function(){
+    var city = $("#city-input").val().replace(/\ /g, "%20");
 
+    console.log(city);
 
-
-fetch(
-    'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely&appid=d3c47a1f177d224c8f7fe16686ddb65e'
-    ).then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-});
-
-fetch("https://api.myptv.com/geocoding/v1/locations/by-text?searchText=Los%20Angeles", {
+    var data = fetch("https://api.myptv.com/geocoding/v1/locations/by-text?searchText=" + city, {
         method: "GET",
         headers: { apiKey: "Y2E4ODI1NGU1MjlhNGFmODllN2VhYTQ0NzM4ZWUzZDM6MjAwYmZlN2UtZWYzNi00ZDIyLTkzNjEtNjFiMGU2MmE4NGY3", "Content-Type": "application/json" },
     })
     .then(response => response.json())
-    .then(result => console.log(result));
+    .then(result => {
+        console.log(result);
+        console.log(result.locations[0].referencePosition);
+        return result.locations[0].referencePosition;
+    })
+    .then(function(data) {
+        // var roundLat = Math.round(data.latitude * 100) / 100;
+        // var roundLong = Math.round(data.longitude * 100) / 100;
+        fetch(
+            'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.latitude + '&lon=' + data.longitude + '&exclude=minutely&appid=d3c47a1f177d224c8f7fe16686ddb65e'
+            ).then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data);
+        });
+    })
 
+    // console.log(data);
+    // .then(function(data) {
+    //     var lat = result[1];
+    //     console.log(lat);
+    // })
 
-$("#searchBtn").on("click", function(){
-    city = $("#city-input").val();
+    
 
-    console.log(city);
+    // var lat = result[0].referencePosition;
+    // var long = result[0].locations.referencePosition.longitude;
+    // console.log(lat);
+    // console.log(long);
 });
+
