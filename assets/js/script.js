d5b616dd55f1cdbf6b23f46c5ed477d19f97ac99
MrSumada <ricksamericana.com>
2022-01-25 15:39:58 -0500

    add localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
-var heading;
+// var heading;
+var savedBtns = JSON.parse(localStorage.getItem("Recent Searches")) || [];
+console.log(savedBtns);
+
+function startUpBtns() {
+    var btnContainer = document.querySelector("#cityBtns");
+    var buttonEl = document.createElement("button");
+    for ( var i = 0; i < savedBtns.length; i++) {
+        buttonEl.className = "cityBtn mw-100 searchBtn";
+        buttonEl.textContent= savedBtns[i].name;
+        btnContainer.appendChild(buttonEl);
+    }
+}
+
+startUpBtns();
+
 
 // fetch(
 //     'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely&appid=d3c47a1f177d224c8f7fe16686ddb65e'
@@ -17,10 +32,18 @@ var heading;
 //     .then(result => console.log(result.locations[0].referencePosition));
 
 
-$("#searchBtn").on("click", function(){
+
+// Click Search Button and initiate Geolocation fetch
+$(".searchBtn").on("click", function(){
 
     // swap spaces with %20 for api call
-    var city = $("#city-input").val().replace(/\ /g, "%20");
+    if ($(this).text() !== "Search") {
+        var city = $(this).text().replace(/\ /g, "%20")
+    } else {
+        var city = $("#city-input").val().replace(/\ /g, "%20");
+    }
+
+    console.log("button clicked");
 
     // API call for lat and long of city
     var data = fetch("https://api.myptv.com/geocoding/v1/locations/by-text?searchText=" + city, {
@@ -33,7 +56,18 @@ $("#searchBtn").on("click", function(){
         // Append City Name from API formatted Name
         heading = result.locations[0].formattedAddress;
         $("#city-name").text(heading).append();
+        
+        if (savedBtns.length < 11){
+            var savedBtn = {
+                name: result.locations[0].formattedAddress,
+                lat: result.locations[0].latitude,
+                long: result.locations[0].longitude
+            };
+            savedBtns.push(savedBtn);
+            localStorage.setItem("Recent Searches", JSON.stringify(savedBtns));
+        }
         return result.locations[0].referencePosition;
+
     })
 
     // API call for weather data
@@ -50,8 +84,13 @@ $("#searchBtn").on("click", function(){
             var timeStamp = data.current.dt + data.timezone_offset + 18000;
             var date = new Date(timeStamp * 1000);
             var weekday = date.getDay();
-            var options = {weekday: "long"}
-            var weekdayNamed = new Intl.DateTimeFormat('en-US', options).format(weekday);
+            if (weekday === 0) { weekdayNamed = "Sunday";}
+            if (weekday === 1) { weekdayNamed = "Monday";}
+            if (weekday === 2) { weekdayNamed = "Tuesday";}
+            if (weekday === 3) { weekdayNamed = "Wednesday";}
+            if (weekday === 4) { weekdayNamed = "Thursday";}
+            if (weekday === 5) { weekdayNamed = "Friday";}
+            if (weekday === 6) { weekdayNamed = "Saturday";}
             var month = date.getMonth() + 1;
             var day = date.getDate();
             var year = date.getFullYear();
@@ -69,8 +108,8 @@ $("#searchBtn").on("click", function(){
             $("#date").text(locationTime).append();
 
             // Append Weather Icon
-            var sunrise = data.current.sunrise;
-            var sunset = data.current.sunset;
+            var sunrise = data.current.sunrise + 18000;
+            var sunset = data.current.sunset + 18000;
             var weather = data.current.weather[0].main;
             var weatherIcon;
             if (weather === "Clouds") {weatherIcon = " ☁️"}
@@ -81,6 +120,8 @@ $("#searchBtn").on("click", function(){
             if (weather === "Mist" || weather === "Smoke" || weather === "Haze" ||weather === "Dust" || weather === "Fog" || weather === "Sand" || weather === "Squall" || weather === "Ash") 
                 {weatherIcon = " 🌫"}
             if (weather === "Clear") {
+                console.log(timeStamp - sunrise);
+                console.log(timeStamp - sunset);
                 if (timeStamp - sunrise < 0 || timeStamp - sunset > 0) { weatherIcon = " 🌑"
                 } else {weatherIcon = " ☀️"}
             }
@@ -117,27 +158,33 @@ $("#searchBtn").on("click", function(){
             }
             $("#uvBtn").text("UV Index: " + uvIndex);
             
+
+            // 5 Day Forecast Loop
             for (var i = 0; i < 5; i++) {
+                var cardId = "#day-" + [i+1];
+
                 var dailyTimeStamp = data.daily[i].dt + data.timezone_offset + 18000;
                 var dailyDate = new Date(dailyTimeStamp * 1000);
-                
-                var options = {weekday: "long"}
-                var dailyWeekdayNamed = new Intl.DateTimeFormat('en-US', options).format(weekday);
+                var dailyWeekday = date.getDay() + i;
+                // var options = {weekday: "long"}
+                // var dailyWeekdayNamed = new Intl.DateTimeFormat('en-US', options).format(weekday);
                             
                 var dailyMonth = dailyDate.getMonth() + 1;
                 var dailyDay = dailyDate.getDate();
                 var dailyYear = dailyDate.getFullYear();
                 var finalDailyDate = dailyMonth + "/" + dailyDay + "/" + dailyYear;
 
-                console.log(weekday)
-
-                var cardId = "#day-" + [i+1];
+                if (dailyWeekday === 0 || dailyWeekday === 7) { dailyWeekdayNamed = "Sunday";}
+                if (dailyWeekday === 1 || dailyWeekday === 8) { dailyWeekdayNamed = "Monday";}
+                if (dailyWeekday === 2 || dailyWeekday === 9) { dailyWeekdayNamed = "Tuesday";}
+                if (dailyWeekday === 3 || dailyWeekday === 10) { dailyWeekdayNamed = "Wednesday";}
+                if (dailyWeekday === 4) { dailyWeekdayNamed = "Thursday";}
+                if (dailyWeekday === 5) { dailyWeekdayNamed = "Friday";}
+                if (dailyWeekday === 6) { dailyWeekdayNamed = "Saturday";}
+                
                 $(cardId).find(".card-weekday").text(dailyWeekdayNamed).append();
                 $(cardId).find(".card-date").text(finalDailyDate).append();
-
-                weekday++;
-                options = {weekday: "long"};
-
+                
                 var dailyWeather = data.daily[i].weather[0].main;
                 var dailyIcon;
                 if (dailyWeather === "Clouds") {dailyIcon = " ☁️"}
@@ -150,7 +197,6 @@ $("#searchBtn").on("click", function(){
                 if (dailyWeather === "Clear") {
                     {dailyIcon = " ☀️"}
                 }
-
                 $(cardId).find(".card-icon").text(dailyIcon).append();
 
                 var dailyTemp = (Math.round(((data.daily[i].temp.day - 273.15) * 1.8 + 32) * 100)) / 100;
@@ -162,6 +208,13 @@ $("#searchBtn").on("click", function(){
                 var dailyHumid = (Math.round(data.current.humidity));
                 $(cardId).find(".card-humid").text("Humidity: " + dailyHumid + "%").append();
             }
+        }).then(function(data){
+            var btnContainer = document.querySelector("#cityBtns")
+            var buttonEl = document.createElement("button");
+            buttonEl.className = "cityBtn mw-100 searchBtn";
+            buttonEl.textContent= heading;
+            btnContainer.appendChild(buttonEl);
+
         })
     })
 
