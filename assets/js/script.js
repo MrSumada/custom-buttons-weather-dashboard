f59f5cb3255e72e85b9dbf83d07965c83160857b
MrSumada <ricksamericana.com>
2022-01-26 05:38:59 -0500

    fix button bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,45 @@
-// var heading;
+var button;
+var saveArray = false;
+var skipConfirm = false;
+
+// Add Recent Buttons from localStorage, OR set savedBtns as blank Array.
 var savedBtns = JSON.parse(localStorage.getItem("Recent Searches")) || [];
 console.log(savedBtns);
 
+    // Append Recent Search Buttons
 function startUpBtns() {
     var btnContainer = document.querySelector("#cityBtns");
-    var buttonEl = document.createElement("button");
+    
     for ( var i = 0; i < savedBtns.length; i++) {
-        buttonEl.className = "cityBtn mw-100 searchBtn";
-        buttonEl.textContent= savedBtns[i].name;
+        var buttonEl = document.createElement("button");
+        buttonEl.className = "cityBtn searchBtn mw-100";
+        buttonEl.textContent = savedBtns[i].name;
+        var btnInput = JSON.stringify(savedBtns[i].input);
+        buttonEl.setAttribute("data-input", btnInput);
+        console.log(btnInput);
         btnContainer.appendChild(buttonEl);
     }
+    btnClicks();
 }
 
+// Add saved Buttons Function
 startUpBtns();
 
-
-// fetch(
-//     'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely&appid=d3c47a1f177d224c8f7fe16686ddb65e'
-//     ).then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(data) {
-//         console.log(data);
-// });
-
-// fetch("https://api.myptv.com/geocoding/v1/locations/by-text?searchText=Los%20Angeles", {
-//         method: "GET",
-//         headers: { apiKey: "Y2E4ODI1NGU1MjlhNGFmODllN2VhYTQ0NzM4ZWUzZDM6MjAwYmZlN2UtZWYzNi00ZDIyLTkzNjEtNjFiMGU2MmE4NGY3", "Content-Type": "application/json" },
-//     })
-//     .then(response => response.json())
-//     .then(result => console.log(result.locations[0].referencePosition));
-
-
-
-// Click Search Button and initiate Geolocation fetch
-$(".searchBtn").on("click", function(){
-
-    // swap spaces with %20 for api call
+function search(event) {
+    // If Recent Search Buttons selected, use their "Input" data, from savedBtns Input property
     if ($(this).text() !== "Search") {
-        var city = $(this).text().replace(/\ /g, "%20")
+        var city = $(this).data("input");
+
+    // If search button selected, use input field for City name
     } else {
-        var city = $("#city-input").val().replace(/\ /g, "%20");
+        // swap spaces with %20 for api call
+        var city = $("#city-input").val().toLowerCase().replace(/\ /g, "%20");
+        $("#city-input").val("");
+        saveArray = true;
     }
 
-    console.log("button clicked");
-
     // API call for lat and long of city
-    var data = fetch("https://api.myptv.com/geocoding/v1/locations/by-text?searchText=" + city, {
+    fetch("https://api.myptv.com/geocoding/v1/locations/by-text?searchText=" + city, {
         method: "GET",
         headers: { apiKey: "Y2E4ODI1NGU1MjlhNGFmODllN2VhYTQ0NzM4ZWUzZDM6MjAwYmZlN2UtZWYzNi00ZDIyLTkzNjEtNjFiMGU2MmE4NGY3", "Content-Type": "application/json" },
     })
@@ -56,18 +49,46 @@ $(".searchBtn").on("click", function(){
         // Append City Name from API formatted Name
         heading = result.locations[0].formattedAddress;
         $("#city-name").text(heading).append();
+
+        // Check if recent Button already exists
+        for (var i = 0; i < savedBtns.length; i++) {
+            var btnCheck = savedBtns[i].name;
+            if (btnCheck === heading) {
+                saveArray = false;
+            }
+        }
         
-        if (savedBtns.length < 11){
+        // If not, Save Button to localStorage
+        if (savedBtns.length < 12 && saveArray === true){
             var savedBtn = {
-                name: result.locations[0].formattedAddress,
-                lat: result.locations[0].latitude,
-                long: result.locations[0].longitude
+                name: heading,
+                input: city
             };
-            savedBtns.push(savedBtn);
-            localStorage.setItem("Recent Searches", JSON.stringify(savedBtns));
+            if (!skipConfirm) {
+                savedBtns.push(savedBtn);
+                localStorage.setItem("Recent Searches", JSON.stringify(savedBtns));
+            }
+        } else if (savedBtns.length = 12 && saveArray === true && skipConfirm !== true) {
+            saveArray = false;
+            var reset = window.confirm("Sorry! You can't add any more Recent Search Buttons. Would you like to reset your recent searches?");
+            if (reset === true) {
+                savedBtns = [];
+                localStorage.setItem("Recent Searches", JSON.stringify(savedBtns));
+                $(".cityBtn").remove();
+                var savedBtn = {
+                    name: heading,
+                    input: city
+                };
+                savedBtns.push(savedBtn);
+                localStorage.setItem("Recent Searches", JSON.stringify(savedBtns));
+                saveArray = true;
+            } else {
+                skipConfirm = true;
+            }
+            
+            
         }
         return result.locations[0].referencePosition;
-
     })
 
     // API call for weather data
@@ -78,7 +99,6 @@ $(".searchBtn").on("click", function(){
             return response.json();
         })
         .then(function(data) {
-            console.log(data);
 
             // Append Current City Time
             var timeStamp = data.current.dt + data.timezone_offset + 18000;
@@ -95,15 +115,17 @@ $(".searchBtn").on("click", function(){
             var day = date.getDate();
             var year = date.getFullYear();
             var hours = date.getHours();
-            var minutes = date.getMinutes();
-                if (minutes < 10) { minutes = "0" + minutes; }
             var amPm;
-            if (hours >= 12) {
+            if (hours > 12) {
                 hours = hours - 12;
                 amPm = "pm"
             } else {
                 amPm = "am"
             }
+            if (hours === 0) { hours = 12; }
+            var minutes = date.getMinutes();
+                if (minutes < 10) { minutes = "0" + minutes; }
+
             var locationTime = "Where it is " + weekdayNamed + ", " + month + "/" + day + "/" + year + ", and the time is " + hours + ":" + minutes + " " + amPm;
             $("#date").text(locationTime).append();
 
@@ -120,8 +142,6 @@ $(".searchBtn").on("click", function(){
             if (weather === "Mist" || weather === "Smoke" || weather === "Haze" ||weather === "Dust" || weather === "Fog" || weather === "Sand" || weather === "Squall" || weather === "Ash") 
                 {weatherIcon = " 🌫"}
             if (weather === "Clear") {
-                console.log(timeStamp - sunrise);
-                console.log(timeStamp - sunset);
                 if (timeStamp - sunrise < 0 || timeStamp - sunset > 0) { weatherIcon = " 🌑"
                 } else {weatherIcon = " ☀️"}
             }
@@ -157,7 +177,6 @@ $(".searchBtn").on("click", function(){
                 $("#uvBtn").addClass("uv-extreme");
             }
             $("#uvBtn").text("UV Index: " + uvIndex);
-            
 
             // 5 Day Forecast Loop
             for (var i = 0; i < 5; i++) {
@@ -208,27 +227,47 @@ $(".searchBtn").on("click", function(){
                 var dailyHumid = (Math.round(data.current.humidity));
                 $(cardId).find(".card-humid").text("Humidity: " + dailyHumid + "%").append();
             }
-        }).then(function(data){
-            var btnContainer = document.querySelector("#cityBtns")
-            var buttonEl = document.createElement("button");
-            buttonEl.className = "cityBtn mw-100 searchBtn";
-            buttonEl.textContent= heading;
-            btnContainer.appendChild(buttonEl);
 
+        //Append new recent search "City" buttons
+        }).then(function(data){
+            if (savedBtns.length <= 12 && saveArray === true && skipConfirm !== true) {
+                var btnContainer = document.querySelector("#cityBtns");
+                var buttonEl = document.createElement("button");
+                buttonEl.className = "cityBtn mw-100 newSearchBtn";
+                buttonEl.textContent= heading;
+                buttonEl.setAttribute("data-input", city);
+                btnContainer.appendChild(buttonEl);
+                $(".newSearchBtn").on("click", search);
+                saveArray = false;
+                console.log("saved")
+            }
         })
     })
+// });
+}
 
-    // console.log(data);
-    // .then(function(data) {
-    //     var lat = result[1];
-    //     console.log(lat);
-    // })
+// Add button querySelectors
+function btnClicks() {
+    button = document.querySelectorAll(".searchBtn");
+}
 
+// Click Search Button and initiate Geolocation fetch
+
+$(button).on("click", search);
+
+
+    // fetch(
+    //     'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely&appid=d3c47a1f177d224c8f7fe16686ddb65e'
+    //     ).then(function(response) {
+    //         return response.json();
+    //     })
+    //     .then(function(data) {
+    //         console.log(data);
+    // });
     
-
-    // var lat = result[0].referencePosition;
-    // var long = result[0].locations.referencePosition.longitude;
-    // console.log(lat);
-    // console.log(long);
-});
-
+    // fetch("https://api.myptv.com/geocoding/v1/locations/by-text?searchText=Los%20Angeles", {
+    //         method: "GET",
+    //         headers: { apiKey: "Y2E4ODI1NGU1MjlhNGFmODllN2VhYTQ0NzM4ZWUzZDM6MjAwYmZlN2UtZWYzNi00ZDIyLTkzNjEtNjFiMGU2MmE4NGY3", "Content-Type": "application/json" },
+    //     })
+    //     .then(response => response.json())
+    //     .then(result => console.log(result.locations[0].referencePosition));
